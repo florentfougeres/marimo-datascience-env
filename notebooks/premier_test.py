86d7901f36c50429f54abacb80f2b0d9f780bf5d
Florent Fougères <florent.fougeres@oslandia.com>
2025-11-25 19:14:37 +0100

    commit

---
+++ notebooks/premier_test.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.18.0"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import streamlit
+    return
+
+
+@app.cell
+def _():
+    a = 1
+    return
+
+
+if __name__ == "__main__":
+    app.run()
